43d8e1192c444949a697684a4d790d57062c9461
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2024-11-09 05:33:24 -0600

    Add Get Geners Method

--- MinimalAPIPeliculas/Program.cs
+++ MinimalAPIPeliculas/Program.cs
@@ -1,6 +1,20 @@
+using MinimalAPIPeliculas.Entidades;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/generos", () =>
+{
+    var generos = new List<Genero>
+    {
+        new Genero { Id = 1, Nombre = "Drama" },
+        new Genero { Id = 2, Nombre = "Acción" },
+        new Genero { Id = 3, Nombre = "Comedia" },
+    };
+
+    return generos;
+});
 
 app.Run();

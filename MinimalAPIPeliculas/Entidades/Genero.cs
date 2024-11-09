c33559ba48df477bf3a267b92369a9fd8ab20eea
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2024-11-09 05:26:56 -0600

    Add Genero class

---
+++ MinimalAPIPeliculas/Entidades/Genero.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MinimalAPIPeliculas.Entidades;
+
+public class Genero
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+
+
+}
--- MinimalAPIPeliculas/obj/Debug/net8.0/MinimalAPIPeliculas.AssemblyInfo.cs
+++ MinimalAPIPeliculas/obj/Debug/net8.0/MinimalAPIPeliculas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalAPIPeliculas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0b1d2be62935b6ff5521e8a91b410bf8bea97f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalAPIPeliculas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalAPIPeliculas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
